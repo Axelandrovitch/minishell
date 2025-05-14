8bb144d675c9936f8e099ede2322a41cd93b38a9
Axel Hetru <ahetru@car12s1.42barcelona.com>
2025-05-14 14:37:45 +0200

    add handle_redirections function; not tested

---
+++ src/execution/hande_redirections.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hande_redirections.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahetru <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/14 12:57:42 by ahetru            #+#    #+#             */
+/*   Updated: 2025/05/14 12:57:43 by ahetru           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../minishell.h"
+#include <unistd.h>
+
+int	is_redirection(t_token *token)
+{
+
+	while (token)
+	{
+		if (token->type == T_REDIR_IN || token->type == T_REDIR_APPEND 
+				|| token->type == T_REDIR_OUT || token->type == T_HEREDOC)
+			return (1);
+		token = token->next;
+	}
+	return (0);
+}
+
+int	handle_fd(enum e_token_type type, char *filename)
+{
+	int	fd;
+
+	fd = -1;
+	if (type == T_REDIR_OUT)
+		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	else if (type == T_REDIR_APPEND)
+		fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	else if (type == T_REDIR_IN)
+		fd = open(filename, O_RDONLY);
+	else if (type == T_HEREDOC)
+	{
+		printf("Heredoc TODO\n");
+		exit(0);
+	}
+	return (fd);
+}
+
+int	handle_redirections(t_command_block *command_block)
+{
+	t_token	*token;
+	t_token	*file_token;
+	int		fd;
+
+	token = command_block->tokens;
+	fd = -1;
+	while (token)
+	{
+		if (is_redirection(token))
+		{
+			file_token = token->next;
+			if (!file_token)
+			{
+				ft_putstr_fd("minishell: syntax error near `newline`\n",STDERR_FILENO);
+				return (1);
+			}
+			if (file_token->type != T_WORD)
+			{
+				ft_putstr_fd("minishell: syntax error near`", 2);
+				ft_putstr_fd(file_token->value, 2);
+				ft_putstr_fd("`\n", 2);
+				return (1);
+			}
+			fd = handle_fd(token->type, file_token->value);
+			if (fd == -1)
+			{
+				perror("open");
+				printf("DEBUG PRINT: Remember to clean befor exit\n");
+				exit(1);
+			}
+			if (token->type == T_REDIR_OUT || token->type == T_REDIR_APPEND)
+				dup2(fd, STDOUT_FILENO);
+			else if (token->type == T_REDIR_IN || token->type == T_HEREDOC)
+				dup2(fd, STDIN_FILENO);
+			close(fd);
+		}
+		token = token->next;
+	}
+	return (0);
+}
