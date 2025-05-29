4b031f56db535f8a41429883116920229e312d04
dcampas <davidcampas30@gmail.com>
2025-05-29 19:16:32 +0200

    smth about SHLVL

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dcampas- <dcampas-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 10:40:07 by dcampas-          #+#    #+#             */
-/*   Updated: 2025/05/29 17:54:24 by dcampas-         ###   ########.fr       */
+/*   Updated: 2025/05/29 19:14:31 by dcampas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,6 +63,54 @@ static void	execute_command_pipeline(t_shell *shell)
 	execute_pipeline(shell);
 }
 
+/* void	ft_setenv(t_shell *shell, const char *key, const char *value)
+{
+	char	*env_var;
+
+	if (!key || !value)
+		return ;
+	env_var = ft_strjoin(key, "=");
+	if (!env_var)
+		return ;
+	env_var = ft_strjoin(env_var, value);
+	if (!env_var)
+		return ;
+	add_or_update_env(&shell->env, env_var, NULL);
+	free(env_var);
+} */
+
+void	update_shlvl(t_shell *shell)
+{
+	char	*shlvl_str;
+	int		shlvl;
+	char	*new_value;
+	char	*args[] = {"unset", "SHLVL", NULL};
+
+	shlvl_str = ft_getenv(shell, "SHLVL");
+	if (!shlvl_str)
+	{
+		update_env_var(shell->env, "SHLVL", "1");
+		return ;
+	}
+	shlvl = ft_atoi(shlvl_str);
+	if (shlvl < 0)
+		shlvl = 0;
+	else if (shlvl >= 999)
+	{
+		printf("minishell: warning: SHLVL too high, resetting to 1\n");
+		shlvl = 1;
+	}
+	else
+		shlvl++;
+	new_value = ft_itoa(shlvl);
+
+	builtin_unset(args, shell->env);
+	update_env_var(shell->env, "SHLVL", new_value);
+	free(new_value);
+}
+
+
+
 int	main(int ac, char **av, char **envp)
 {
 	t_shell	shell;
@@ -75,6 +123,7 @@ int	main(int ac, char **av, char **envp)
 
 	shell.last_exit_status = 0;
 	init_minishell(&shell, envp);
+	update_shlvl(&shell);
 	setup_interactive_signals();
 	while (1)
 	{
