dd0ebbfa3d71efbb9727202ec7cda2d3eacb08d9
dcampas <davidcampas30@gmail.com>
2025-06-04 14:11:32 +0200

    norm libft

--- libft/src/ft_str/ft_strjoin_free_s1.c
+++ libft/src/ft_str/ft_strjoin_free_s1.c
@@ -6,7 +6,7 @@
 /*   By: dcampas- <dcampas-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 18:36:13 by dcampas-          #+#    #+#             */
-/*   Updated: 2025/05/13 18:38:15 by dcampas-         ###   ########.fr       */
+/*   Updated: 2025/06/04 14:10:13 by dcampas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 char	*ft_strjoin_free_s1(char *s1, char *s2)
 {
 	char	*result;
-	
+
 	result = ft_strjoin(s1, s2);
-	free(s1);
+	free (s1);
 	return (result);
 }
--- libft/src/ft_to/ft_atol.c
+++ libft/src/ft_to/ft_atol.c
@@ -6,7 +6,7 @@
 /*   By: dcampas- <dcampas-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 12:55:21 by dcampas-          #+#    #+#             */
-/*   Updated: 2025/05/14 12:57:13 by dcampas-         ###   ########.fr       */
+/*   Updated: 2025/06/04 14:10:51 by dcampas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,18 @@ long	ft_atol(const char *str)
 	result = 0;
 	sign = 1;
 	i = 0;
-
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 		i++;
-
 	if (str[i] == '+' || str[i] == '-')
 	{
 		if (str[i] == '-')
 			sign = -1;
 		i++;
 	}
-
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		result = result * 10 + (str[i] - '0');
 		i++;
 	}
-
 	return (sign * result);
 }
